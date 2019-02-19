c590457539d57f3d6715ebae7bb4241fac00b256
Xiaobin <xshen1@statestreet.com>
2019-02-19 13:21:52 +0800

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 const gulp = require('gulp')
-const { watch } = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins')
 const ts = require('gulp-typescript');
 const less = require('gulp-less');
@@ -8,7 +7,7 @@ const tsProject = ts.createProject('tsconfig.json');
 const plugins = gulpLoadPlugins()
 const env = process.env.NODE_ENV || 'development'
 const isProduction = () => env === 'production'
-
+const path = require('path')
 // gulp.task('default', () => {
 //     return gulp.src(['src/**/*.tsx'])
 //       .pipe(plugins.sourcemaps.init())
@@ -17,28 +16,59 @@ const isProduction = () => env === 'production'
 //       .pipe(gulp.dest('dist'))
 // })
 
+gulp.task('clean', cb => {
+    rmDir('dist')
+    cb()
+})
+
 gulp.task('compile:less', () => {
-    gulp.src(['src/lime.less'])
-      .pipe(less())
-      .pipe(gulp.dest('dist'))
+    return gulp.src(['src/lime.less'])
+        .pipe(less())
+        .pipe(gulp.dest('dist'))
 })
 
 gulp.task('compile:tsx', () => {
-    gulp.src(['src/**/*.tsx','src/index.js','src/index.css'])
+    return gulp.src(['src/**/*.tsx', 'src/index.js', 'src/index.css'])
         .pipe(tsProject())
         .js.pipe(gulp.dest('dist'));
-})  
-
-gulp.task('copy:package', () => {
-    copyFile('package.json','dist/package.json')
 })
 
-gulp.task('default', ['compile:less','compile:tsx','copy:package'])
-
-watch(['src/**/*.js', 'src/**/*.less','src/**/*.tsx','src/**/*.ts'], () => {
-    gulp.start('default')
+gulp.task('copy:package', cb => {
+    copyFile('package.json', 'dist/package.json')
+    cb()
 })
 
-function copyFile(source,target) {
+gulp.task('default', ['clean','compile:less', 'compile:tsx', 'copy:package'])
+
+gulp.watch(['src/**/*.js', 'src/**/*.less', 'src/**/*.tsx', 'src/**/*.ts'], ['clean','compile:less', 'compile:tsx', 'copy:package'])
+
+function copyFile(source, target) {
     fs.createReadStream(source).pipe(fs.createWriteStream(target));
+}
+
+function rmDir(dirPath, removeSelf = false) {
+    let files
+    try {
+        files = fs.readdirSync(dirPath)
+    }
+    catch (e) {
+        console.exception(e)
+        return;
+    }
+
+    if (files.length > 0) {
+        for (let i = 0; i < files.length; i++) {
+            let filePath = path.join(dirPath, files[i])
+            if (fs.statSync(filePath).isFile()) {
+                fs.unlinkSync(filePath);
+            }
+            else {
+                rmDir(filePath, true)
+            }
+        }
+    }
+
+    if (removeSelf) {
+        fs.rmdirSync(dirPath)
+    }
 }
