184b29d7dba5a53705e6eb6156dbded6e342e103
Xiaobin <xshen1@statestreet.com>
2019-02-28 18:17:36 +0800

    sp

--- src/Layer/index.tsx
+++ src/Layer/index.tsx
@@ -1,18 +1,31 @@
 import * as classnames from 'classnames'
 import * as React from 'react'
+import { prefixCls } from '../common';
 
 export interface ILayerProps {
     boundingClientRect?: { top, right, bottom, left, width, height }
-    left?: number,
-    top?: number,
-    width?: number,
     className?: string
     height?: number
+    left?: number,
+    mask?: boolean,
     onBlur: (evt?) => void,
     show?: boolean
+    top?: number,
+    width?: number,
 }
 
 export default class Layer extends React.PureComponent<ILayerProps, {}> {
+    private ref: React.RefObject<HTMLDivElement>
+
+    static defaultProps = {
+        mask: false
+    }
+
+    constructor(props) {
+        super(props)
+        this.ref = React.createRef()
+    }
+
     get style() {
         let {
             boundingClientRect: rect,
@@ -28,22 +41,41 @@ export default class Layer extends React.PureComponent<ILayerProps, {}> {
         return style
     }
 
+    private onWindowMouseDown = evt => {
+        let { target } = evt
+        let { onBlur = () => {} } = this.props
+        if(this.ref.current && !this.ref.current.contains(target)) {
+            //console.log(`blur`,)
+            onBlur(evt)
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('mousedown', this.onWindowMouseDown)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('mousedown', this.onWindowMouseDown)
+    }
+
     render() {
         let {
             boundingClientRect: rect,
             children,
             className,
             left,
+            mask,
             top,
             width,
             height,
             onBlur,
             show } = this.props
-        let classes = classnames('sd-layer', className)
+        let classes = classnames(`${prefixCls}-layer`, className)
 
         return show ? [
-            <div key={0} className='sd-layer-mask' onClick={onBlur}></div>,
+            mask && <div key={0} className={`${prefixCls}-layer-mask`} ></div>,
             <div key={1}
+                ref={this.ref}
                 className={classes}
                 style={this.style}
             >
