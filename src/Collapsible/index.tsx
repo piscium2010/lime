c51064f5ef930e1fa89d83c36ce0affb20eb4fd5
Xiaobin <xshen1@statestreet.com>
2019-03-04 13:40:56 +0800

    add collapsible animation

--- src/Collapsible/index.tsx
+++ src/Collapsible/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
 import { prefixCls } from '../common';
 
 export interface ICollapsibleProps extends React.AllHTMLAttributes<HTMLDivElement> {
@@ -13,12 +12,10 @@ export interface ICollapsibleState {
 
 export default class Collapsible extends React.PureComponent<ICollapsibleProps, ICollapsibleState> {
     private ref: React.RefObject<HTMLDivElement>
-    private contentRef: React.RefObject<HTMLDivElement>
 
     constructor(props) {
         super(props)
         this.ref = React.createRef()
-        this.contentRef = React.createRef()
         this.state = {
             expand: props.expand || props.defaultExpand || false
         }
@@ -28,82 +25,51 @@ export default class Collapsible extends React.PureComponent<ICollapsibleProps, 
         return this.props.expand || this.state.expand
     }
 
-    private get maxHeight(): number | string {
-        return this.contentRef.current && this.contentRef.current.offsetHeight || 1000
+    private get node(): HTMLElement {
+        return this.ref.current
     }
 
     private performEnterAnimation = () => {
-        let node = this.contentRef.current
-        let height = node.offsetHeight
-        //let node = this.contentRef.current.offsetHeight
-        node.style.height = '0px'
-        setTimeout(() => {
-            console.log(`timeout1`,)
-            node.style.height = `${height}px`
-        }, 17)
-        setTimeout(() => {
-            console.log(`timeout2`,)
-            node.style.height = ``
-        }, 3000)
-       
-        // setTimeout(() => {
-        //     node.style.height = `${height}px`
-        // }, 17)
-        //this.contentRef.current.style.height = `${h}px`
+        this.node.style.display = 'block'
+        let height = this.node.offsetHeight
+        let handleTransitionEnd = () => {
+            this.node.style.height = ``
+            this.node.removeEventListener('transitionend', handleTransitionEnd)
+        }
+        this.node.addEventListener('transitionend', handleTransitionEnd)
+        this.node.style.height = '0px'
+        setTimeout(() => { this.node.style.height = `${height}px` }, 17)
+    }
 
+    private performLeaveAnimation = () => {
+        let height = this.node.offsetHeight
+        let handleTransitionEnd = () => {
+            this.node.style.display = 'none'
+            this.node.style.height = ``
+            this.node.removeEventListener('transitionend', handleTransitionEnd)
+        }
+        this.node.addEventListener('transitionend', handleTransitionEnd)
+        this.node.style.height = `${height}px`
+        setTimeout(() => { this.node.style.height = `0px` }, 17)
+    }
+
+    componentDidMount() {
+        this.node.style.display =  this.expand ? 'block' : 'none'
     }
 
     componentDidUpdate() {
-        let node = this.ref.current
-        if (this.expand && node.childElementCount === 0 && this.props.children) {
-            console.log(`expand`)
-            ReactDOM.render(
-                <div ref={this.contentRef} className={`${prefixCls}-collapsible-content`}>
-                    {this.props.children}
-                </div>,
-                node
-            )
-            setTimeout(this.performEnterAnimation, 0)
-            //this.performEnterAnimation()
-        } else if (node.childElementCount) {
-            console.log(`collapse`)
-            ReactDOM.unmountComponentAtNode(node)
+        if (this.expand && this.node.childElementCount && this.node.style.display == 'none') {
+            this.performEnterAnimation()
+        } else if (this.node.childElementCount) {
+            this.performLeaveAnimation()
         }
-
-        //this.contentRef.current.style.height = `${h}px`
-        // if(this.expand) {
-        //     console.log(`epand`,)
-        //     let h = this.contentRef.current.offsetHeight
-        //     console.log(`h`,h)
-        //     setTimeout(()=>{
-
-        //     }, 0)
-        //     this.contentRef.current.style.height = '0px'
-        //     setTimeout(()=>{
-        //         this.contentRef.current.style.height = `${h}px`
-
-        //     }, 17)
-        //     //this.contentRef.current.style.height = `${h}px`
-        // }
     }
 
     render() {
         return (
-            // <div className={`${prefixCls}-collapsible`} aria-expanded={this.expand}
-            //     style={{ height: this.expand ? this.maxHeight : 0}}>
-            //     <div ref={this.contentRef}>
-            //         {this.props.children}
-            //     </div>
-            // </div>
-            // <div className={`${prefixCls}-collapsible`} aria-expanded={this.expand}>
-            //     {
-            //         this.expand &&
-            //         <div ref={this.contentRef} className={`${prefixCls}-collapsible-content`}>
-            //             {this.props.children}
-            //         </div>
-            //     }
-            // </div>
-            <div ref={this.ref} className={`${prefixCls}-collapsible`} aria-expanded={this.expand}></div>
+            <div ref={this.ref} className={`${prefixCls}-collapsible`} aria-expanded={this.expand}>
+                {this.props.children}
+            </div>
         )
     }
 }
