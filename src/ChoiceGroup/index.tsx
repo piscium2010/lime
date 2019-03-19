a06928c1282e8e13a1242ec9bfc5d7ef748f54f5
Xiaobin <xshen1@statestreet.com>
2019-03-19 21:23:10 +0800

    sp

--- src/ChoiceGroup/index.tsx
+++ src/ChoiceGroup/index.tsx
@@ -1,22 +1,23 @@
 import * as classnames from 'classnames'
 import * as React from 'react'
-import Dropdown from '../Dropdown/index'
+import { prefixCls } from '../common/index'
 
-type Option = {key, value}
+export type Option = {key, value}
 
-type Props = {
+export interface IChoiceGroupProps {
     className?: string
     defaultSelectedKey?: string | number
+    name?: string
     onChange?: (option?: Option) => void
     options: Array<Option>
     selectedKey?: string | number
 }
 
-type State = {
+export interface IChoiceGroupState {
     selectedKey: string | number
 }
 
-export default class ChoiceGroup extends React.PureComponent<Props, State> {
+export default class ChoiceGroup extends React.PureComponent<IChoiceGroupProps, IChoiceGroupState> {
     public static defaultProps = {
         onChange: () => {}
     }
@@ -28,15 +29,26 @@ export default class ChoiceGroup extends React.PureComponent<Props, State> {
         }
     }
 
+    private get selectedKey() {
+        return 'selectedKey' in this.props ? this.props.selectedKey : this.state.selectedKey
+    }
+
+    private onClickChoice = (option: Option) => {
+        this.setState({
+            selectedKey: option.key
+        })
+        this.props.onChange(option)
+    }
+
     public render() {
-        const { className, options, ...props } = this.props
-        const classes = classnames('lime-choice-group', className)
+        const { className, name = '', options, onChange, ...rest } = this.props
+        const classes = classnames(`${prefixCls}-choice-group`, className)
 
         return (
-            <div className={classes}>
+            <div className={classes} {...rest}>
                 {
                     options.map(option => {
-                        const choiceClasses = classnames('lime-choice', {
+                        const choiceClasses = classnames(`${prefixCls}-choice`, {
                             selected: option.key == this.selectedKey
                         })
                         return (
@@ -44,25 +56,14 @@ export default class ChoiceGroup extends React.PureComponent<Props, State> {
                                 className={choiceClasses}
                                 onClick={evt => this.onClickChoice(option)}
                             >
-                                <div className='lime-choice-radio'></div>
-                                <div className='lime-choice-text'>{option.value}</div>
+                                <div className={`${prefixCls}-choice-radio`}></div>
+                                <div className={`${prefixCls}-choice-text`}>{option.value}</div>
+                                <input type="radio" name={name} value={option.value}></input>
                             </div>
                         )
                     })
                 }
             </div>
         )
-    }
-
-    private onClickChoice(option: Option) {
-        const { onChange } = this.props
-        this.setState({
-            selectedKey: option.key
-        })
-        onChange(option)
-    }
-
-    private get selectedKey() {
-        return this.props.selectedKey || this.state.selectedKey
     }
 }
