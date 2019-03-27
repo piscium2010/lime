1f8340836f79bb16503bb5440652589aeb2c7004
Xiaobin <xshen1@statestreet.com>
2019-03-27 16:30:01 +0800

    sp

--- src/Checkbox/index.tsx
+++ src/Checkbox/index.tsx
@@ -2,16 +2,18 @@ import * as classnames from 'classnames'
 import * as React from 'react'
 import { prefixCls } from '../common/index'
 
-export interface ICheckboxProps extends React.AllHTMLAttributes<HTMLDivElement> {
+export interface ICheckboxProps {
     boxStyle?: React.CSSProperties
     className?: string
+    checked: boolean,
     defaultChecked?: boolean
     label?: string
     name?: string
-    onChange?: Function
+    onChange?: Function,
+    onClick: any
 }
 
-export interface ICheckboxState {
+interface ICheckboxState {
     checked: boolean
 }
 
@@ -33,26 +35,24 @@ export default class Checkbox extends React.PureComponent<ICheckboxProps, ICheck
     }
 
     private onClick = evt => {
-        const { checked } = this.state
-        this.setState({ checked: !checked })
-        this.props.onChange({ name: this.props.name, checked: !checked })
-        this.props.onClick(evt)
+        // console.log(`cpature`,)
+        // const { checked } = this.state
+        // this.setState({ checked: !checked })
+        // this.props.onChange({ name: this.props.name, checked: !checked })
+        // this.props.onClick(evt)
     }
 
     public render() {
         const { checked } = this.state
-        const { boxStyle, className, label, name = '', onChange, onClick, ...rest } = this.props
+        const { boxStyle, className, label, name = '', onChange, ...rest } = this.props
         const classes = classnames(`${prefixCls}-checkbox-wrapper`, className)
         const boxClasses = classnames(`${prefixCls}-checkbox`, { checked })
-
         return (
             <div className={classes} onClickCapture={this.onClick} {...rest}>
                 <span className={boxClasses} style={boxStyle}>
                     <input className={`${prefixCls}-checkbox-input`} type='checkbox' name={name} checked={this.checked} />
                 </span>
-                {
-                    label && <label className={`${prefixCls}-checkbox-label`} for={name} >{label}</label>
-                }
+                {label && <label className={`${prefixCls}-checkbox-label`}>{label}</label>}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import Button from './Button'
-import Card from './Card'
 import Checkbox from './Checkbox'
 import ChoiceGroup from './ChoiceGroup'
 import Dialog from './Dialog'
 import Collapsible from './Collapsible'
 
 export {
-    Button,
-    Card,
     Checkbox,
     ChoiceGroup,
     Collapsible,
